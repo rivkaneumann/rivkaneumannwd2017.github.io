435dd31dd7bb02ccbbe8fbc63ebfbe41b95b1b23
Rivka Neumann <Rivka Neumann>
2016-12-16 08:36:23 +0200

    assignment 4

--- classes/class2/assets/jquery_test.js
+++ classes/class2/assets/jquery_test.js
@@ -1,27 +1,27 @@
 •	$(function(){
 
         var captionHidden = true;
-//
-//        $("img").on("click", function(){
-//
-//            if (captionHidden){
-//                $(this).next().show();
-//                captionHidden = false;
-//            }else{
-//
-//                $(this).next().hide()
-//                captionHidden = true;
-//            }
-//        });
 
-    var myImg = $("img");
-    var myCaption = myImg.next();
-    myImg.on("mouseenter", function(){
-        myCaption.slideDown();
-    }).on("mouseleave", function(){
-        myCaption.slideUp();
-    });
-});
+        $("img").on("click", function(){
+
+            if (captionHidden){
+                $(this).next().show();
+                captionHidden = false;
+            }else{
+
+                $(this).next().hide()
+                captionHidden = true;
+            }
+        });
+
+//    var myImg = $("img");
+//    var myCaption = myImg.next();
+//    myImg.on("mouseenter", function(){
+//        myCaption.slideDown();
+//    }).on("mouseleave", function(){
+//        myCaption.slideUp();
+//    });
+//});
 
 
 
