99d9fefd5b4e91070ff23adcf03a108770c590a5
Rivka Neumann <Rivka Neumann>
2016-12-21 18:30:54 +0200

    class5work

--- classes/class5/main.js
+++ classes/class5/main.js
@@ -1,11 +1,31 @@
 $(function(){
-    var $myNavItem = $("nav> ul > li");
-    var $myDropDown = $myNavItem.find("div")
 
-    myNavItem.on("mouseover", function(){
+    var $myNavItem = $("nav > ul > li");
+    //var $myDropDown = $myNavItem.find("div");
 
-        $(this).find("div").slideDown();
+    $myNavItem.on("mouseover", function(){
+        $(this).find("div").animate(
+            {height: "100px"},
+            {queue: false,
+             duration: 500,
+             easing: "linear",
+             start: function(){
+                 $(this).css("border-width", "3px");
+             }
+            }
+        );
+
     }).on("mouseleave", function(){
-        $(this).find("div").dlideToggle();
+        $(this).find("div").animate(
+            {height: "0px"},
+            {queue: false,
+             duration: 500,
+             easing: "linear",
+             complete: function(){
+                 $(this).css("border-width","0");
+             }
+            }
+
+        );
     });
 });
